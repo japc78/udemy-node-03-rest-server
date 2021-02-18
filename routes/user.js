ba1514d657caf4dc424d42b9a2b381841d0deeda
japc <japc.grafico@gmail.com>
2021-02-18 13:22:23 +0100

    Validation role from database

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { userGet, userPost, userPatch, userPut, userDelete } = require('../controllers/user');
 
-const { validFields } = require('../middlewares/valid-fields')
+const { validFields } = require('../middlewares/valid-fields');
+const Role = require('../models/role');
 
 const router = Router();
 
@@ -11,7 +12,14 @@ router.post('/', [
     check('email', 'The Email is not valid').isEmail(),
     check('name', 'The Name is required').not().isEmpty(),
     check('password', 'The password must be at least 6 characters long').isLength({min: 6}),
-    check('role', 'It is not a valid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('role', 'It is not a valid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('role').custom( async (role = '') => {
+        const roleExists = await Role.findOne({ role });
+
+        if (!roleExists) {
+           throw new Error(`The role: ${role} is not register in Database`);
+        }
+    }),
     validFields
 ], userPost);
 router.put('/:id', userPut);
