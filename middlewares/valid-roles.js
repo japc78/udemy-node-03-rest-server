c5eea6f127f3a589159ad2d843c06a92d7086edb
japc <japc.grafico@gmail.com>
2021-02-23 18:04:11 +0100

    Middleware for validate more than one role

--- middlewares/valid-roles.js
+++ middlewares/valid-roles.js
@@ -13,6 +13,20 @@ const isAdminRole = (req = request, res = response, next) => {
     next();
 }
 
+
+const shouldBeRole = ( ...roles ) => {
+    return (req = request, res = response, next) => {
+        if (!req.user) {
+            return res.status(500).json({ msg: 'Is necessary to valid the toke first'});
+        }
+
+        const { role, name } = req.user;
+        if (!roles.includes(role)) return res.status(401).json({ msg: `The user: ${name} has not authorized role. it Should have: ${roles}` });
+        next();
+    }
+}
+
 module.exports = {
-    isAdminRole
+    isAdminRole,
+    shouldBeRole
 }
