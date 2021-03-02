8dd58627170ea7d56b9c5e5db542d6ebe0460080
japc <japc.grafico@gmail.com>
2021-03-02 16:29:29 +0100

    Search user by id

--- controllers/search.js
+++ controllers/search.js
@@ -1,13 +1,50 @@
 const { request, response } = require("express");
+const { ObjectId } = require ('mongoose').Types;
+
+const { User, Product, Category} = require('../models');
+
+const allowCollection = [
+    'categories',
+    'products',
+    'roles',
+    'users',
+]
+
 
 const search = ( req = request, res = response) => {
     const { collection, term } = req.params;
 
-    res.json({
-        msg: 'Search',
-        collection,
-        term
-    })
+    if (!allowCollection.includes(collection)) return res.status(400).json({ msg: `The collection allow are: ${allowCollection}`});
+
+    switch (collection) {
+        case 'categories':
+
+        break;
+
+        case 'products':
+
+        break;
+
+        case 'roles':
+
+        break;
+
+        case 'users':
+            searchUsers(term, res);
+        break;
+    }
+}
+
+const searchUsers = async ( term = '',  res = response ) => {
+    const isMongoId = ObjectId.isValid(term);
+
+    if (isMongoId) {
+        const user = await User.findById(term);
+
+        return res.json({
+            results: (user) ? [user] : []
+        });
+    }
 }
 
 module.exports = {
